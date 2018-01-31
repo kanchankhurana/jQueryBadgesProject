d0edd306705c163c7ab9c18d31a6534381358994
Temp <temp@GODEL0569.local>
2018-01-31 15:44:52 +0530

    jquery badge project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
+  $.ajax({
+    url: "https://www.codeschool.com/users/Kk2989.json",
+    dataType: 'jsonp',
+    success: function(response){
+      var data = response.courses.completed;
+      console.log(data);
 
-  // your code will go here
+      for(var i = 0; i < data.length; i++){
+        console.log(data.length);
 
+        var courseDiv = $('<div />',{
+          'class':'course'
+        }).appendTo('#badges');
+
+        var dataTitle = data[i].title;
+        var dataImg = data[i].badge;
+        var dataUrl = data[i].url;
+
+        var courseHeading = $('<h3 />',{
+          text: dataTitle
+        }).appendTo(courseDiv);
+
+        var courseImg = $('<img />',{
+          src: dataImg
+        }).appendTo(courseDiv);
+
+        var courseLink = $('<a />',{
+          'class': 'btn btn-primary',
+          target: '_blank',  
+          href: dataUrl,
+          text: 'See Course'
+        }).appendTo(courseDiv); 
+      }
+    }
+  });
 });
+
+
